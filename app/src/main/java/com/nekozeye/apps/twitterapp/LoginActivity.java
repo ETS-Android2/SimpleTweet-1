7279828b75abe4a51326613d6dbc3b554993bb16
nekozye <nekozeye@gmail.com>
2022-02-11 01:20:09 -0500

    renaming packages to custom fit

--- app/src/main/java/com/nekozeye/apps/twitterapp/LoginActivity.java
+++ app/src/main/java/com/nekozeye/apps/twitterapp/LoginActivity.java
@@ -1,15 +1,17 @@
-package com.codepath.apps.restclienttemplate;
+package com.nekozeye.apps.twitterapp;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
-import com.codepath.apps.restclienttemplate.models.SampleModel;
-import com.codepath.apps.restclienttemplate.models.SampleModelDao;
+import com.codepath.apps.twitterapp.R;
+import com.nekozeye.apps.twitterapp.models.SampleModel;
+import com.nekozeye.apps.twitterapp.models.SampleModelDao;
 import com.codepath.oauth.OAuthLoginActionBarActivity;
 
-public class LoginActivity extends OAuthLoginActionBarActivity<RestClient> {
+public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 
 	SampleModelDao sampleModelDao;
 	
@@ -21,7 +23,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<RestClient> {
 		final SampleModel sampleModel = new SampleModel();
 		sampleModel.setName("CodePath");
 
-		sampleModelDao = ((RestApplication) getApplicationContext()).getMyDatabase().sampleModelDao();
+		sampleModelDao = ((TwitterApp) getApplicationContext()).getMyDatabase().sampleModelDao();
 
 		AsyncTask.execute(new Runnable() {
 			@Override
@@ -43,6 +45,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<RestClient> {
 	// i.e Display application "homepage"
 	@Override
 	public void onLoginSuccess() {
+		Log.i("TwitterApplication","login Success");
 		// Intent i = new Intent(this, PhotosActivity.class);
 		// startActivity(i);
 	}
---
+++ app/src/main/java/com/nekozeye/apps/twitterapp/TimelineActivity.java
@@ -0,0 +1,16 @@
+package com.nekozeye.apps.twitterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codepath.apps.twitterapp.R;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+    }
+}
